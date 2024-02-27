33fdc77286daafe8448fd8e8b8bbc83e199df2b2
devops-team-92 <44020182+devops-team-92@users.noreply.github.com>
2024-02-27 18:14:49 +0530

    t

--- vars/shellcommand.groovy
+++ vars/shellcommand.groovy
@@ -3,5 +3,5 @@ import com.example.newClass2
 
 def call(String command) {
     def output = new newClass2()
-    def output = output.executeShell(command)
+    output = output.executeShell(command)
 }
